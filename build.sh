997d58b63627d940b133141e40d032843f3a304a
Alexander Engström <aleen361@student.liu.se>
2023-06-19 16:18:47 +0200

    added more rotors

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++17 -Wall -Wextra -Wpedantic -g main.cpp enigma.cpp rotor.cpp reflector.cpp plugboard.cpp
--- main.cpp
+++ main.cpp
@@ -2,22 +2,31 @@
 #include "plugboard.h"
 #include "rotor.h"
 #include "reflector.h"
+
 #include <string>
-#include <iostream>
 #include <vector>
+#include <iostream>
 
 int main() {
   Plugboard plugboard{std::vector<std::string>{"AB", "CD", "DF"}};
-  Rotor rotor_i{"EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q'};
-  Rotor rotor_ii{"AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E'};
+
+  // More historical rotors and reflectors can be found here:
+  // https://en.wikipedia.org/wiki/Enigma_rotor_details
+  
+  Rotor rotor_i  {"EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q'};
+  Rotor rotor_ii {"AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E'};
   Rotor rotor_iii{"BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V'};
+  Rotor rotor_iv {"ESOVPZJAYQUIRHXLNFTGKDCMWB", 'J'};
+  Rotor rotor_v  {"VZBRGITYUPSDNHLXAWMJQOFECK", 'Z'};
+  
   Reflector reflector_a{"EJMZALYXVBWFCRQUONTSPIKHGD"};
   Reflector reflector_b{"YRUHQSLDPXNGOKMIEBFZCWVJAT"};
+  Reflector reflector_c{"FVPJIAOYEDRZXWGCTKUQSBNMHL"};
 
   std::vector<Rotor> rotors{rotor_i, rotor_ii, rotor_iii};
   
   Enigma enigma{reflector_b, rotors, plugboard};
   std::vector<int> ring_setting{3,2,3};
-  std::cout << enigma.encrypt("SMPQPTFGM", "CAT", ring_setting) << std::endl;
+  std::cout << enigma.encrypt("SUPERSECRETMESSAGE", "XYZ", ring_setting) << std::endl;
   return 0;
 }
